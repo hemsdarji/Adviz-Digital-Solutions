24bf84ec4092bdaa9c3bda67cc168f1d06ff69bf
hemsdarji <hemsdarji8160304190@gmail.com>
2023-09-12 22:26:36 +0530

    modified-homesection

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,122 +1,143 @@
 import React from "react";
 import "./Home.css";
 import { Link } from "react-router-dom";
-import { serviceCards ,ServiceCard } from "../../ServiceCard";
+import { serviceCards, ServiceCard } from "../../ServiceCard";
 import Team from "../Team/Team";
 
-
-
+const imgArr = [
+  "./img/re1.jpg",
+  "/img/re2.jpg",
+  "/img/re3.jpg",
+  "/img/re4.jpg",
+  "/img/re1.png",
+]
 
 const Home = () => {
   return (
     <>
-    <section className="hero">
-    <div className="container">
-      <div className="row">
-      <div className="left-div col-md-12 col-lg-6">
-          <h1>Welcome to Adviz Social Media Marketing Agency</h1>
-          <p>
-            Your Success, Our Expertise <br />
-            "Fueling your social success! 🚀 Unleash the power of strategic content, engagement wizardry, and trend mastery. Let's skyrocket your brand together! 🌟 #AdvizSocial"
-          </p>
-          <Link to="/Contact" className="btn btn-secondary ">
-            Contact Us
-          </Link>
-        </div>
-        <div className="right-div col-md-12 col-lg-6">
-          <div className="image-container" style={{ maxWidth: "100%" }}>
-            <img
-              src="./img/digital-marketing-main.svg"
-              alt="image"
-              className="img-fluid"
+      <section className="hero">
+        <div className="container">
+          <div className="row">
+            <div className="left-div col-md-12 col-lg-6">
+              <h1>Welcome to Adviz Social Media Marketing Agency</h1>
+              <p>
+                Your Success, Our Expertise <br />
+                "Fueling your social success! 🚀 Unleash the power of strategic
+                content, engagement wizardry, and trend mastery. Let's skyrocket
+                your brand together! 🌟 #AdvizSocial"
+              </p>
+              <Link to="/Contact" className="btn btn-secondary ">
+                Contact Us
+              </Link>
+            </div>
+            <div className="right-div col-md-12 col-lg-6">
+              <div className="image-container" style={{ maxWidth: "100%" }}>
+                <img
+                  src="./img/digital-marketing-main.svg"
+                  alt="image"
+                  className="img-fluid"
+                  style={{ width: "100%", height: "auto" }}
+                />
+              </div>
+            </div>
+          </div>
+          <div className="custom-shape-divider-bottom-1693225745">
+            <svg
+              data-name="Layer 1"
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 1200 120"
+              preserveAspectRatio="none"
               style={{ width: "100%", height: "auto" }}
-            />
+            >
+              <path
+                d="M0,0V46.29c47.79,22.2,103.59,32.17,158,28,70.36-5.37,136.33-33.31,206.8-37.5C438.64,32.43,512.34,53.67,583,72.05c69.27,18,138.3,24.88,209.4,13.08,36.15-6,69.85-17.84,104.45-29.34C989.49,25,1113-14.29,1200,52.47V0Z"
+                opacity=".25"
+                class="shape-fill"
+              ></path>
+              <path
+                d="M0,0V15.81C13,36.92,27.64,56.86,47.69,72.05,99.41,111.27,165,111,224.58,91.58c31.15-10.15,60.09-26.07,89.67-39.8,40.92-19,84.73-46,130.83-49.67,36.26-2.85,70.9,9.42,98.6,31.56,31.77,25.39,62.32,62,103.63,73,40.44,10.79,81.35-6.69,119.13-24.28s75.16-39,116.92-43.05c59.73-5.85,113.28,22.88,168.9,38.84,30.2,8.66,59,6.17,87.09-7.5,22.43-10.89,48-26.93,60.65-49.24V0Z"
+                opacity=".5"
+                class="shape-fill"
+              ></path>
+              <path
+                d="M0,0V5.63C149.93,59,314.09,71.32,475.83,42.57c43-7.64,84.23-20.12,127.61-26.46,59-8.63,112.48,12.24,165.56,35.4C827.93,77.22,886,95.24,951.2,90c86.53-7,172.46-45.71,248.8-84.81V0Z"
+                class="shape-fill"
+              ></path>
+            </svg>
           </div>
         </div>
-      </div>
-      <div className="custom-shape-divider-bottom-1693225745">
-        <svg
-          data-name="Layer 1"
-          xmlns="http://www.w3.org/2000/svg"
-          viewBox="0 0 1200 120"
-          preserveAspectRatio="none"
-          style={{ width: "100%", height: "auto" }}
-        >
-        <path d="M0,0V46.29c47.79,22.2,103.59,32.17,158,28,70.36-5.37,136.33-33.31,206.8-37.5C438.64,32.43,512.34,53.67,583,72.05c69.27,18,138.3,24.88,209.4,13.08,36.15-6,69.85-17.84,104.45-29.34C989.49,25,1113-14.29,1200,52.47V0Z" opacity=".25" class="shape-fill"></path>
-        <path d="M0,0V15.81C13,36.92,27.64,56.86,47.69,72.05,99.41,111.27,165,111,224.58,91.58c31.15-10.15,60.09-26.07,89.67-39.8,40.92-19,84.73-46,130.83-49.67,36.26-2.85,70.9,9.42,98.6,31.56,31.77,25.39,62.32,62,103.63,73,40.44,10.79,81.35-6.69,119.13-24.28s75.16-39,116.92-43.05c59.73-5.85,113.28,22.88,168.9,38.84,30.2,8.66,59,6.17,87.09-7.5,22.43-10.89,48-26.93,60.65-49.24V0Z" opacity=".5" class="shape-fill"></path>
-        <path d="M0,0V5.63C149.93,59,314.09,71.32,475.83,42.57c43-7.64,84.23-20.12,127.61-26.46,59-8.63,112.48,12.24,165.56,35.4C827.93,77.22,886,95.24,951.2,90c86.53-7,172.46-45.71,248.8-84.81V0Z" class="shape-fill"></path>
-    </svg>
-</div>
+      </section>
 
-      </div>
-    </section>
-    
-<section className=" services-section">
-    <div className="container text-center common-title fw-bold">
-        <h2 className="common-heading">What We Will Do For <br /> Your Business</h2>
-        <hr className="w-25 mx-auto " />
-    </div>
+      <section className=" services-section">
+        <div className="container text-center common-title fw-bold">
+          <h2 className="common-heading">
+            What We Will Do For <br /> Your Business
+          </h2>
+          <hr className="w-25 mx-auto " />
+        </div>
 
-    <div className="container mt-10">
-      <div className="row g-5">
-        {serviceCards.map((card, index) => (
+        <div className="container mt-10">
+          <div className="row g-5">
+            {serviceCards.map((card, index) => (
+              <ServiceCard
+                key={index}
+                imageSrc={card.imageSrc}
+                title={card.title}
+                description={card.description}
+              />
+            ))}
+          </div>
+        </div>
+      </section>
 
-          <ServiceCard
-            key={index}
-            imageSrc={card.imageSrc}
-            title={card.title}
-            description={card.description}
-          />
-
-        ))}
-      </div>
-    </div>
-
-
-</section>
-
-<section className="carousel">
-<div className="container text-center common-title fw-bold">
-        <h2 className="common-heading">Visible Successes
-</h2>
-        <hr className="w-25 mx-auto " />
-    </div>
-  <div className="in-img">
-  <div id="carouselExample" className="carousel slide">
-    <div className="carousel-inner">
-      <div className="carousel-item active">
-        <img src="./img/re1.jpg" className="d-block w-100" alt="..." />
-      </div>
-      <div className="carousel-item">
-        <img src="/img/re2.jpg" className="d-block w-100" alt="..." />
-      </div>
-      <div className="carousel-item">
-        <img src="/img/re3.jpg" className="d-block w-100" alt="..." />
-      </div>
-      <div className="carousel-item">
-        <img src="/img/re4.jpg" className="d-block w-100" alt="..." />
-      </div>
-      <div className="carousel-item">
-        <img src="/img/re1.png" className="d-block w-100" alt="..." />
-      </div>
-    </div>
-    <button className="carousel-control-prev" type="button" data-bs-target="#carouselExample" data-bs-slide="prev">
-      <span className="carousel-control-prev-icon" aria-hidden="true"></span>
-      <span className="visually-hidden">Previous</span>
-    </button>
-    <button className="carousel-control-next" type="button" data-bs-target="#carouselExample" data-bs-slide="next">
-      <span className="carousel-control-next-icon" aria-hidden="true"></span>
-      <span className="visually-hidden">Next</span>
-    </button>
-  </div>
-  <section>
-    <Team />
-  </section>
-  </div>
-  
-</section>
-
-
+      <section className="carousel">
+        <div className="container text-center common-title fw-bold">
+          <h2 className="common-heading">Visible Successes</h2>
+          <hr className="w-25 mx-auto " />
+        </div>
+        <div className="in-img">
+        <div id="carouselExample" className="carousel slide">
+            <div className="carousel-inner">
+              {imgArr.map((image, index) => (
+                <div
+                  key={index}
+                  className={`carousel-item ${index === 0 ? "active" : ""}`}
+                >
+                  <img src={image} className="d-block w-100" alt={`Image ${index + 1}`} />
+                </div>
+              ))}
+            </div>
+            <button
+              className="carousel-control-prev"
+              type="button"
+              data-bs-target="#carouselExample"
+              data-bs-slide="prev"
+            >
+              <span
+                className="carousel-control-prev-icon"
+                aria-hidden="true"
+              ></span>
+              <span className="visually-hidden">Previous</span>
+            </button>
+            <button
+              className="carousel-control-next"
+              type="button"
+              data-bs-target="#carouselExample"
+              data-bs-slide="next"
+            >
+              <span
+                className="carousel-control-next-icon"
+                aria-hidden="true"
+              ></span>
+              <span className="visually-hidden">Next</span>
+            </button>
+          </div>
+          
+        </div>
+      </section>
+      <section>
+            <Team />
+          </section>
     </>
   );
 };
